a4d646d1d37777446f640b6d3d7ef05ba08c344b
Anthony Fletcher <afletcher2433.com>
2023-11-01 17:37:05 -0400

    new commit

--- app.py
+++ app.py
@@ -52,9 +52,85 @@ def estimate_loss():
     out = {}
     model.eval() 
     for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters) 
+        losses = torch.zeros(eval_iters)   # possibly eval_interval for this and 56
         for k in range(eval_iters):
             X, Y = get_batch(split)  
             logits, loss = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean() 
+
+#simple bigram model
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx)    # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T) 
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            
+            #get the predictions
+            
+            logits, loss = self(idx)
+            
+            # focus only on the last time step
+            
+            logits = logits[:, -1, :]   # becomes (B, C)
+            
+            #apply softmax to get probabilities
+
+            probs = F.softmax(logits, dim=1)    # (B, C) 
+
+            # sample from the distribution
+
+            idx_next = torch.multinomial(probs, num_samples=1)  #(B, 1) 
+
+            #append sampled index to the running sequence
+
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1) 
+        return idx
+ 
+model = BigramLanguageModel(vocab_size)
+m = model.to(device) 
+
+# We will create a PyTorch optimizer for effiency
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate) 
+
+for iter in range(max_iters): 
+
+    #every once in a while evaluate the loss on train and val sets
+    if iter % eval_intervals == 0:
+        losses = estimate_loss() 
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}") 
+
+    # sample a batch of data
+    xb, yb = get_batch('train') 
+
+    # evaluate the loss
+    logits, loss = model(xb, yb) 
+    optimizer.zero_grad(set_to_none=True)
+    loss.backwards() 
+    optimizer.step() 
+
+# Generate from the model
+context= torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500[0].tolist())))
